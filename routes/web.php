24a6b78445b4a096f78dece02f911919b9a9c9e7
Aji Pamungkas <Jimungkas40@gmail.com>
2023-05-30 18:38:20 +0700

    final commit

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,14 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-Route::resource('books', BookController::class);
+// Route::resource('books', BookController::class);
+Route::get('/', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
+Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+
 
 
